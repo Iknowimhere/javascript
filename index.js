dfbfa1d6c16541453877b2e0da5a27b91f67478d
umashankar <umashankarp33@gmail.com>
2024-10-16 08:43:46 +0530

    string

---
+++ index.js
@@ -0,0 +1,35 @@
+// let chocolatePrice=20;
+// let quantity=10;
+// let res=`The total price of ${quantity} chocolates is ${chocolatePrice*quantity}`;
+// console.log(res);
+
+
+
+// let a='hello world';
+// let a='200';
+// console.log(a.slice(-5,-3));
+// console.log(a.substring(0,7));
+// console.log(a.substring(-5,-3));
+// console.log(a.indexOf('l'));
+// console.log(a.lastIndexOf('l'));
+// console.log(a.length);
+// console.log(a.charAt(2));
+// console.log(a.concat('hello'));
+// console.log(a.charCodeAt(2));
+// console.log(a.includes('hell'));
+// console.log(a.padStart(6,'₹'));
+// console.log(a.padEnd(6,'₹'));
+// let a='hello world';
+// console.log(a.split(' '));
+// let a='hello world';
+// console.log(a.endsWith('d'));
+// console.log(a.startsWith('d'));
+
+
+
+
+
+
+
+
+
